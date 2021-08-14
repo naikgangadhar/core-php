9bbf8db8bbbefaf770ad9059effdd9ae45d0def1
Ganga <naikgangadhar@github.com>
2021-08-14 22:55:57 +0530

    master | gitigonre

--- settings.php
+++ settings.php
@@ -11,5 +11,5 @@ ini_set("log_errors", TRUE);
 ini_set('error_log', ERROR_LOG);
 if (!defined('SERVERNAME')) define('SERVERNAME', "localhost");
 if (!defined('USER')) define('USER', "root");
-if (!defined('PASSWORD')) define('PASSWORD', "Mo0rd@831");
+if (!defined('PASSWORD')) define('PASSWORD', "*****");
 if (!defined('DBNAME')) define('DBNAME', "coredb");
